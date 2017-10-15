8cc51b0ea372ef35e2e6bb62779b5ebcca5e81b0
Makoto Ito <3mdevmail@gmail.com>
2017-10-15 22:46:44 +0900

    :books: 松屋のメニュー情報とランダムに返すAPIを追加

---
+++ api/random.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var all = JSON.parse(fs.readFileSync('./menu/all.json', 'utf8'));
+
+router.get('/', function (req, res, next) {
+  var length = Object.keys(all).length;
+  var random = Math.floor(Math.random() * (length + 1 - 0)) + 0;
+  res.send(all[random]);
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+//API読み込み
+var random = require('./api/random');
+
 var app = express();
 
 // view engine setup
@@ -18,22 +21,25 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/random', random);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
