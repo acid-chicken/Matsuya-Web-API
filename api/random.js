63257c437a3b93288ac0c754b590313279cf7cfb
Makoto Ito <3mdevmail@gmail.com>
2017-11-17 00:26:07 +0900

    :memo: データをJSONで返すように

--- api/random.js
+++ api/random.js
@@ -6,7 +6,8 @@ var all = JSON.parse(fs.readFileSync('./menu/all.json', 'utf8'));
 router.get('/', function (req, res, next) {
   var random = Math.floor(Math.random() * (45 + 1 - 0)) + 0;
   console.log(random + '/' + 45);
-  res.send(all[random]);
+  res.header("Content-Type", "application/json; charset=utf-8");
+  res.send("[\"" + all[random] + "\"]");
 });
 
 module.exports = router;
