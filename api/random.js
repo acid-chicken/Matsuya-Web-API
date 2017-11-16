9445d5383a2a3b81fe3bb1f10974bc934fb03edb
Makoto Ito <3mdevmail@gmail.com>
2017-11-17 00:51:16 +0900

    :shirt: JSONの呼び出し方を変更

--- api/random.js
+++ api/random.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var all = JSON.parse(fs.readFileSync('./menu/all.json', 'utf8'));
+var all = JSON.parse(fs.readFileSync('menu/all.json', 'utf8'));
 
 router.get('/', function (req, res, next) {
   var random = Math.floor(Math.random() * (44 + 1 - 0)) + 0;
