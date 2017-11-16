e513a332afa0565e1f29f72ed128b272ce53ba8d
Makoto Ito <3mdevmail@gmail.com>
2017-11-17 00:03:28 +0900

    :books: 全メニューの順番変更とランダムAPIでサイドメニューを返さないように

--- api/random.js
+++ api/random.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var all = JSON.parse(fs.readFileSync('../menu/all.json', 'utf8'));
+var all = JSON.parse(fs.readFileSync('./menu/all.json', 'utf8'));
 
 router.get('/', function (req, res, next) {
-  var length = Object.keys(all).length;
-  var random = Math.floor(Math.random() * (length + 1 - 0)) + 0;
-  console.log(random + '/' + length);
+  var random = Math.floor(Math.random() * (45 + 1 - 0)) + 0;
+  console.log(random + '/' + 45);
   res.send(all[random]);
 });
 
