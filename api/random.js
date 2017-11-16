3228fd1204ff60e7b1342609d26099eebc75b291
Makoto Ito <3mdevmail@gmail.com>
2017-11-17 00:34:39 +0900

    :memo: 細かな修正

--- api/random.js
+++ api/random.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var all = JSON.parse(fs.readFileSync('../menu/all.json', 'utf8'));
+var all = JSON.parse(fs.readFileSync('./menu/all.json', 'utf8'));
 
 router.get('/', function (req, res, next) {
-  var length = Object.keys(all).length;
-  var random = Math.floor(Math.random() * (length + 1 - 0)) + 0;
-  console.log(random + '/' + length);
-  res.send(all[random]);
+  var random = Math.floor(Math.random() * (44 + 1 - 0)) + 0;
+  console.log(random + '/' + 44);
+  res.header("Content-Type", "application/json; charset=utf-8");
+  res.send("[\"" + all[random] + "\"]");
 });
 
 module.exports = router;
