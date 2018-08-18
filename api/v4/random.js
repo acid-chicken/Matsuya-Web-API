7fd2092cfd29c90963dfa41c929217d113e220a2
Makoto Ito <3mdevmail@gmail.com>
2018-08-18 15:10:35 +0900

    :tada: 正規表現で指定可能に

--- api/v4/random.js
+++ api/v4/random.js
@@ -1,19 +1,19 @@
 import express from "express";
-import findRandom from "../../helper/findRandom";  
+import findRandom from "../../helper/findRandom";
 const router = express.Router();
 
 router.get('/', async function (req, res) {
   const query = req.query;
   if (query.type) {
     const type = query.type.split(',');
-    const q = { type: type };
+    const q = { type: new RegExp(type) };
     await findRandom(q)
       .then(menu =>res.send(menu))
       .catch(ex => res.send(ex));
   }
   else if (query.name) {
     const name = query.name.split(',');
-    const q = { name: name };
+    const q = { type: new RegExp(name) };
     await findRandom(q)
       .then(menu =>res.send(menu))
       .catch(ex => res.send(ex));
--- api/v4/search.js
+++ api/v4/search.js
@@ -1,19 +1,19 @@
 import express from "express";
-import dbSearch from "../../helper/dbSearch";  
+import dbSearch from "../../helper/dbSearch";
 const router = express.Router();
 
 router.get('/', async function (req, res) {
   const query = req.query;
   if (query.type) {
     const type = query.type.split(',');
-    const q = { type: type };
+    const q = { type: new RegExp(type) };
     await dbSearch(q)
       .then(menu =>res.send(menu))
       .catch(ex => res.send(ex));
   }
   else if (query.name) {
     const name = query.name.split(',');
-    const q = { name: name };
+    const q = { name: new RegExp(name) };
     await dbSearch(q)
       .then(menu =>res.send(menu))
       .catch(ex => res.send(ex));
