a413fe8fc27c0574b87697b1d2e04103827e1e8a
TinyKitten <catwalk8205@gmail.com>
2019-01-29 23:58:47 +0900

    :bug: imageURLが返されないバグを修正

--- model/menu.go
+++ model/menu.go
@@ -13,15 +13,15 @@ func (r *MatsuyaMenu) Marshal() ([]byte, error) {
 }
 
 type MatsuyaMenu struct {
-	Name           string  `json:"name"`
-	Type           string  `json:"type"`
-	Price          int     `json:"price"`
-	Calorie        int     `json:"calorie"`
-	Protein        float64 `json:"protein"`
-	Lipid          float64 `json:"lipid"`
-	Carbohydrate   float64 `json:"carbohydrate"`
-	Sodium         int     `json:"sodium"`
-	SaltEquivalent float64 `json:"saltEquivalent"`
-	Description    string  `json:"description"`
-	ImageURL       string  `json:"imageURL"`
+	Name           string  `name:"name" json:"name"`
+	Type           string  `bson:"type" json:"type"`
+	Price          int     `bson:"price" json:"price"`
+	Calorie        int     `bson:"calorie" json:"calorie"`
+	Protein        float64 `bson:"protein" json:"protein"`
+	Lipid          float64 `bson:"lipid" json:"lipid"`
+	Carbohydrate   float64 `bson:"carbohydrate" json:"carbohydrate"`
+	Sodium         int     `bson:"sodium" json:"sodium"`
+	SaltEquivalent float64 `bson:"saltEquivalent" json:"saltEquivalent"`
+	Description    string  `bson:"description" json:"description"`
+	ImageURL       string  `bson:"imageURL" json:"imageURL"`
 }
